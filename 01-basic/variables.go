96926d4a8d4df8cf1b63bedca05cd93ca62f1c68
sam <belonk@126.com>
2021-07-16 20:54:21 +0800

    add Go Lang test code of variables.

--- 01-basic/variables.go
+++ 01-basic/variables.go
@@ -1,23 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // 变量
 func main() {
 	// ==============
-	// 变量申明
+	// 变量声明
 	// ==============
 
-	// 变量申明：var 变量名 变量类型
+	// 变量声明：var 变量名 变量类型
 	var v0 rune   // 字符
 	var v1 int    // 整型
 	var v2 string // 字符串
 	// 变量赋值
 	v0 = 'A'
-	print("v0=", v0, " v1=", v1, " v2=", v2) // v0=65 v1=0 v2=
-	println()
+	fmt.Println("v0=", v0, "v1=", v1, "v2=", v2) // ~: v0= 65 v1= 0 v2=
 
-	// 可以将多个变量合并申明
+	// 可以将多个变量合并声明
 	var (
 		v3 [2]int                 // 数组
 		v4 []int                  // 数组切片
@@ -26,18 +27,43 @@ func main() {
 		v7 map[string]int         // map
 		v8 func(a int, b int) int // 匿名函数
 	)
-	fmt.Print("v3=", v3, " v4=", v4, " v5=", v5, " v6=", v6, " v7=", v7, " v8=", v8)
-	// v3=[0 0] v4=[] v5={0} v6=<nil> v7=map[] v8=<nil>
+	fmt.Println("v3=", v3, "v4=", v4, "v5=", v5, "v6=", v6, "v7=", v7, "v8=", v8)
+	// ~: v3=[0 0] v4=[] v5={0} v6=<nil> v7=map[] v8=<nil>
 
 	// ==============
 	// 变量初始化
 	// ==============
 
+	var v9 int = 10 // 声明变量时初始化赋值
+	var v10 = 10    // 自动推导类型
+	v11 := 10       // 简写方式，省略var关键字，且可推导类型
+	v12 := "abc"    // 声明一个string变量
+	// v9 := 100       // v9变量已经声明过了，编译出错：no new variables on left side of :=
+	fmt.Println("v9=", v9, "v10=", v10, "v11=", v11, "v12=", v12)
+	// ~: v9= 10 v10= 10 v11= 10 v12= abc
+
 	// ==============
 	// 变量赋值
 	// ==============
 
+	var v13 int         // 声明变量
+	v13 = 10            // 变量赋值
+	v14 := 11           // 声明并赋值
+	v13, v14 = v14, v13 // 使用多重赋值交换两个变量的值，不需要引入中间变量
+	fmt.Println("v13=", v13, "v14=", v14)
+	// ~: v13= 11 v14= 10
+
 	// ==============
 	// 匿名变量
 	// ==============
+
+	f, l, n := getName()                                         // 调用函数，返回多个值
+	fmt.Println("firstName=", f, "lastName=", l, "nickName=", n) // ~; firstName= sam lastName= sune nickName= belonk
+	// 如果不需要某些值，可以使用匿名变量将其忽略
+	_, _, nickName := getName()
+	fmt.Println("nickName=", nickName) // ~: nickName= belonk
+}
+
+func getName() (firstName, lastName, nickName string) {
+	return "sam", "sune", "belonk"
 }
