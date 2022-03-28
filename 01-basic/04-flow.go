a4300d127dcab0361798de2ad0465c7e94553ba2
sam <koobyte@126.com>
2022-03-28 10:48:27 +0800

    重构代码

--- 01-basic/04-flow.go
+++ 01-basic/04-flow.go
@@ -17,67 +17,30 @@ func main() {
 	// 左花括号{必须与if或者else处于同一行;
 	// 在if之后，条件语句之前，可以添加变量初始化语句，使用;间隔;
 
-	// 随机产生一个数
-	a := randInt(20)
-	fmt.Println(a)
-	if a > 10 { // if语句不需要圆括号，但是必须要大括号
-		fmt.Println("a > 10")
-	} else if a == 10 {
-		fmt.Println("a == 10")
-	} else {
-		fmt.Println("a < 10")
-	}
-
-	// 条件语句可以初始化，分好分隔
-	if b := 0; b < 1 {
-		fmt.Println("b < 1")
-	}
-
-	r := ifReturn(1)
-	fmt.Println(r) // ~: 1
+	ifFlow()
 
 	// ==============
 	// 选择
 	// ==============
 
-	// go的选择语句关键字包括：switch、case和select，select配合channel使用
-	// 注意switch语句中每一个case分支不需要break语句来跳出，go默认执行分支会自动跳出
+	switchFlow()
 
-	i1 := randInt(3)
-	// > 常规用法
-	switch i1 {
-	case 0:
-		fmt.Println("i1 == 0") // switch不需要break
-	case 1:
-		fmt.Println("i1 == 1")
-	case 2:
-		fmt.Println("i1 == 2")
-	default: // default分支可以省略
-		fmt.Println("Unknown int i1")
-	}
-
-	// > 用法2：switch后可以不跟条件表达式，此时switch相当于简写的if语句
-	switch {
-	case i1 == 0:
-		fmt.Println("i1 is 0.")
-	case i1 > 0:
-		fmt.Println("i1 is great than 0.")
-		//default: // default语句可以省略
-	}
-
-	// > 如果需要case语句继续向下执行，需要使用fallthrough关键字
-	switch i1 {
-	case 0:
-		fmt.Println("i1 is 0")
-	case 1:
-		fallthrough // 符合判断条件时，继续向下执行
-	case 2:
-		fmt.Println("i1 is great than 0")
-	}
 	// ==============
 	// 循环
 	// ==============
 
+	forFlow()
+
+	// ==============
+	// 跳转
+	// ==============
+
+	// 调整语句，关键字包括goto
+	// 使用goto语句实现for循环的效果
+	gotoWhile(5) // ~: 1 2 3 4 5
+}
+
+func forFlow() {
 	// 循环语句，关键字包括for和range
 	// Go语言只有for循环，不支持while和do while，因为for同样可以实现while的效果
 
@@ -139,14 +102,64 @@ TAG:
 	}
 	// ~: j=0 0 i, j=0 1 i, j=0 2 i, j=0 3 i, j=1 0 i, j=1 1
 	fmt.Println()
+}
 
-	// ==============
-	// 跳转
-	// ==============
+func ifFlow() {
+	// 随机产生一个数
+	a := randInt(20)
+	fmt.Println(a)
+	if a > 10 { // if语句不需要圆括号，但是必须要大括号
+		fmt.Println("a > 10")
+	} else if a == 10 {
+		fmt.Println("a == 10")
+	} else {
+		fmt.Println("a < 10")
+	}
 
-	// 调整语句，关键字包括goto
-	// 使用goto语句实现for循环的效果
-	gotoWhile(5) // ~: 1 2 3 4 5
+	// 条件语句可以初始化，分好分隔
+	if b := 0; b < 1 {
+		fmt.Println("b < 1")
+	}
+
+	r := ifReturn(1)
+	fmt.Println(r) // ~: 1
+}
+
+func switchFlow() {
+	// go的选择语句关键字包括：switch、case和select，select配合channel使用
+	// 注意switch语句中每一个case分支不需要break语句来跳出，go默认执行分支会自动跳出
+
+	i1 := randInt(3)
+	// > 常规用法
+	switch i1 {
+	case 0:
+		fmt.Println("i1 == 0") // switch不需要break
+	case 1:
+		fmt.Println("i1 == 1")
+	case 2:
+		fmt.Println("i1 == 2")
+	default: // default分支可以省略
+		fmt.Println("Unknown int i1")
+	}
+
+	// > 用法2：switch后可以不跟条件表达式，此时switch相当于简写的if语句
+	switch {
+	case i1 == 0:
+		fmt.Println("i1 is 0.")
+	case i1 > 0:
+		fmt.Println("i1 is great than 0.")
+		//default: // default语句可以省略
+	}
+
+	// > 如果需要case语句继续向下执行，需要使用fallthrough关键字
+	switch i1 {
+	case 0:
+		fmt.Println("i1 is 0")
+	case 1:
+		fallthrough // 符合判断条件时，继续向下执行
+	case 2:
+		fmt.Println("i1 is great than 0")
+	}
 }
 
 func gotoWhile(stop int) {
