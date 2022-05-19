ae6e62ad773c3410dbb59322e5c521ce52f3dd6c
sam <koobyte@126.com>
2022-05-19 17:24:41 +0800

    add gin, generic and test demo

---
+++ gin-demo/server/server.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"gin-demo/web"
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	gin.DisableConsoleColor()
+	gin.SetMode("debug")
+
+	r := web.InitRouter()
+	// 按目录加载模板
+	r.LoadHTMLGlob("templates/**/*")
+	// 按文件加载模板
+	// r.LoadHTMLFiles("templates/index.html")
+
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
+}
---
+++ gin-demo/starter.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"gin-demo/server"
+)
+
+func main() {
+	server.Start()
+}
