d9c795c0b9be64d9791934887b7fff3f3990b3b2
sam <koobyte@126.com>
2022-05-25 13:43:59 +0800

    m

--- json/json_test.go
+++ json/json_test.go
@@ -3,12 +3,9 @@ package json
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/tidwall/gjson"
 	"testing"
 	"time"
 )
-
-const str = `{"name":{"first":"Janet","last":"Prichard"},"age":47,"pi":3.1415926}`
 
 type T struct {
 	Name Name    `json:"name"`
@@ -71,22 +68,4 @@ func TestFrom(t *testing.T) {
 		return
 	}
 	fmt.Printf("%v", tt)
-}
-
-func TestGjsonFrom(t *testing.T) {
-	value := gjson.Get(str, "name.last")
-	println(value.String())
-}
-
-func TestGjsonFrom1(t *testing.T) {
-	// var m map[string]any = make(map[string]any)
-	result := gjson.Parse(str).Get("name").Get("last")
-	println(result.String())
-
-	m, ok := gjson.Parse(str).Value().(map[string]any)
-	if !ok {
-		// not a map
-	} else {
-		fmt.Printf("%v", m)
-	}
 }
