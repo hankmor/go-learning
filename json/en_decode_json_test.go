26f5def7dd2e3f83d2e323e8699f21441aa6142f
sam <koobyte@126.com>
2022-05-25 16:45:14 +0800

    json demo

---
+++ json/en_decode_json_test.go
@@ -0,0 +1,37 @@
+package json
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
+
+// func NewDecoder(r io.Reader) *Decoder
+// func NewEncoder(w io.Writer) *Encoder
+func TestStreaming(t *testing.T) {
+	s := `{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`
+	var b bytes.Buffer
+	b.Write([]byte(s))
+	// dec := json.NewDecoder(os.Stdin)
+	dec := json.NewDecoder(&b)
+	enc := json.NewEncoder(os.Stdout)
+	for {
+		var v map[string]interface{}
+		// 解码输入的 json 到 map
+		if err := dec.Decode(&v); err != nil {
+			log.Println(err)
+			return
+		}
+		// 打印map
+		for k := range v {
+			fmt.Printf("key: %v, value: %v\n", k, v[k])
+		}
+		// 将解码后的 map 写入 os.Stdout 输出流中
+		if err := enc.Encode(&v); err != nil {
+			log.Println(err)
+		}
+	}
+}
