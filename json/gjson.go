e42819315f5a5baa8ed51edb4d01dfcc8306e836
huzhou <huzhou.v@gmail.com>
2022-05-25 13:43:59 +0800

    m

--- json/gjson.go
+++ json/gjson.go
@@ -1,1 +1,27 @@
 package json
+
+import (
+	"fmt"
+	"github.com/tidwall/gjson"
+	"testing"
+)
+
+const str = `{"name":{"first":"Janet","last":"Prichard"},"age":47,"pi":3.1415926}`
+
+func TestGjsonFrom(t *testing.T) {
+	value := gjson.Get(str, "name.last")
+	println(value.String())
+}
+
+func TestGjsonFrom1(t *testing.T) {
+	// var m map[string]any = make(map[string]any)
+	result := gjson.Parse(str).Get("name").Get("last")
+	println(result.String())
+
+	m, ok := gjson.Parse(str).Value().(map[string]any)
+	if !ok {
+		// not a map
+	} else {
+		fmt.Printf("%v", m)
+	}
+}
