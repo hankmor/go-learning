e0f6a6173bd23cc44679901d0d0efc3ba30e7fc0
huzhou <huzhou.v@gmail.com>
2022-12-26 19:35:30 +0800

    modified

--- goroutine/chan/chan_count.go
+++ goroutine/chan/chan_count.go
@@ -6,40 +6,26 @@ import (
 )
 
 var cnt1 int
+var sg sync.WaitGroup
 
 func main() {
-	// ch := make(chan int)
-	// for i := 0; i < 10; i++ {
-	// 	go countAdd1(ch) // 加1
-	// }
-	// time.Sleep(1 * time.Second)
-	// for v := range ch {
-	// 	fmt.Println(v)
-	// }
-	// close(ch)
-	// fatal error: all goroutines are asleep - deadlock!
-
-	var sg sync.WaitGroup
 	sg.Add(2)
 	ch := make(chan int)
-	for i := 0; i < 10; i++ {
-		go countAdd1(ch) // 加1
-		if i == 9 {
-			sg.Done()
+	go countAdd1(ch) // 增加
+	go func() {      // 读取
+		for v := range ch {
+			fmt.Println("received: ", v)
 		}
-	}
-	// time.Sleep(1 * time.Second)
-	for v := range ch {
-		fmt.Println("received: ", v)
-		if v == 10 {
-			close(ch)
-			sg.Done()
-		}
-	}
+		sg.Done() // 读取任务完成
+	}()
 	sg.Wait()
 }
 
 func countAdd1(ch chan<- int) {
-	cnt1++
-	ch <- cnt1 // 写回通道
+	for i := 0; i < 1000; i++ {
+		cnt1++
+		ch <- cnt1 // 写回通道
+	}
+	close(ch) // 增加完成，关闭通道
+	sg.Done() // 任务完成
 }
