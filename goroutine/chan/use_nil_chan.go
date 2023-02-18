ddef85ccf6c5cd0862dbff62843b71a77b816550
huzhou <huzhou.v@gmail.com>
2023-02-18 23:11:03 +0800

    use nil chan

---
+++ goroutine/chan/use_nil_chan.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 想要 2 个 goroutine 依次输出 2 3 后退出
+func main() {
+	// bad()
+	betterWithNilChan()
+}
+
+func bad() {
+	var c1, c2 = make(chan int), make(chan int)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- 2
+		close(c1) // 关闭 chan，导致 select 读取时不会阻塞
+	}()
+
+	go func() {
+		time.Sleep(time.Second * 3)
+		c2 <- 3
+		close(c2)
+	}()
+
+	var a, b bool
+	for {
+		select {
+		case i := <-c1: // 先输出 2，然后关闭。关闭了的chan读取不会阻塞，此时 i 为零值 0，所以一直输出 0
+			a = true
+			fmt.Println(i)
+		case j := <-c2:
+			b = true
+			fmt.Println(j)
+		}
+		time.Sleep(time.Millisecond * 100)
+		if a && b {
+			break
+		}
+	}
+	/*output:
+	2
+	0
+	0
+	...
+	3
+	*/
+}
+
+func betterWithNilChan() {
+	var c1, c2 = make(chan int), make(chan int)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- 2
+		close(c1) // 关闭 chan，导致 select 读取时不会阻塞
+	}()
+
+	go func() {
+		time.Sleep(time.Second * 3)
+		c2 <- 3
+		close(c2)
+	}()
+
+	for {
+		select {
+		case i, ok := <-c1:
+			if !ok {
+				c1 = nil // 置为 nil，此时 select 从 nil的chan读取会阻塞
+			} else {
+				fmt.Println(i)
+			}
+		case j, ok := <-c2:
+			if !ok {
+				c2 = nil
+			} else {
+				fmt.Println(j)
+			}
+		}
+		time.Sleep(time.Millisecond * 100)
+		if c1 == nil && c2 == nil {
+			break
+		}
+	}
+	/*output:
+	2
+	3
+	*/
+}
