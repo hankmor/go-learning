f94783303eb647f391f788dc02cd32d47352fb1d
huzhou <huzhou.v@gmail.com>
2023-03-10 13:25:07 +0800

    rsa

---
+++ crypto/rsa/rsa_demo.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	CheckError(err)
+
+	fmt.Println("privateKey:", privateKey)
+	publicKey := privateKey.PublicKey
+	fmt.Println("publicKey:", publicKey)
+	secretMessage := "This is super secret message!"
+
+	encryptedMessage := RSA_OAEP_Encrypt(secretMessage, publicKey)
+
+	fmt.Println("Cipher Text:", encryptedMessage)
+
+	RSA_OAEP_Decrypt(encryptedMessage, *privateKey)
+}
+
+func CheckError(e error) {
+	if e != nil {
+		fmt.Println(e.Error)
+	}
+}
+
+func RSA_OAEP_Encrypt(secretMessage string, key rsa.PublicKey) string {
+	label := []byte("OAEP Encrypted")
+	rng := rand.Reader
+	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rng, &key, []byte(secretMessage), label)
+	CheckError(err)
+	return base64.StdEncoding.EncodeToString(ciphertext)
+}
+
+func RSA_OAEP_Decrypt(cipherText string, privKey rsa.PrivateKey) string {
+	ct, _ := base64.StdEncoding.DecodeString(cipherText)
+	label := []byte("OAEP Encrypted")
+	rng := rand.Reader
+	plaintext, err := rsa.DecryptOAEP(sha256.New(), rng, &privKey, ct, label)
+	CheckError(err)
+	fmt.Println("Plaintext:", string(plaintext))
+	return string(plaintext)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/huzhouv/go-learning
 
 go 1.19
+
+require (
+	github.com/ftauth/ftauth v0.0.0-20220117175059-fbe9a17e5b9e
+	golang.org/x/sync v0.1.0
+)
