431129e36c569dc10a3d6e7d87afa0d5e80e9f4b
hank <hankmo.x@gmail.com>
2023-07-18 00:00:13 +0800

    add cli demo

--- 03-cliapp/cli/basic_demo/command/say_hello.go
+++ 03-cliapp/cli/basic_demo/command/say_hello.go
@@ -9,45 +9,6 @@ import (
 
 var weathers = []string{"sunny", "windy", "cloudy", "rainy"}
 
-//func HelloCmd() *cli.Command {
-//	return &cli.Command{
-//		Name:    "hello",        // 命令名称，执行时需要指定
-//		Aliases: []string{"ho"}, // 命令别名，简化名称
-//		Usage:   "向您问好，-h 查看更多帮助信息",
-//		Before: func(context *cli.Context) error {
-//			fmt.Println("sayHello 命令 Before...")
-//			return nil
-//		},
-//		Flags: []cli.Flag{
-//			&cli.StringFlag{Name: "n", Aliases: []string{"name"}, Usage: "您的姓名 `NAME`", Required: true},
-//		},
-//		Subcommands: cli.Commands{
-//			&cli.Command{
-//				Name:    "weatherCmd",     // 命令名称，执行时需要指定
-//				Aliases: []string{"w"}, // 命令别名，简化名称
-//				Usage:   "报告天气情况，-h 查看更多帮助信息",
-//				Before: func(context *cli.Context) error {
-//					fmt.Println("sayHello weatherCmd 子命令 Before...")
-//					return nil
-//				},
-//				Flags: []cli.Flag{},
-//				Action: func(ctx *cli.Context) error {
-//					name := ctx.String("n")
-//					rd := rand.New(rand.NewSource(time.Now().UnixNano()))
-//					weatherCmd := weathers[rd.Intn(len(weathers))]
-//					fmt.Printf("hello %s, today is a %s day!", name, weatherCmd)
-//					return nil
-//				},
-//			},
-//		}, // 子命令
-//		Action: func(ctx *cli.Context) error { // 具体命令的执行逻辑
-//			name := ctx.String("n")
-//			fmt.Println("hello,", name, "!")
-//			return nil
-//		},
-//	}
-//}
-
 var beforeSayHello = func(ctx *cli.Context) error {
 	fmt.Println("Before command sayHello...")
 	return nil
--- 03-cliapp/cli/basic_demo/main.go
+++ 03-cliapp/cli/basic_demo/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"cli_demo/basic_demo/command"
 	"fmt"
 	"github.com/urfave/cli/v2"
+	"math/rand"
 	"os"
+	"time"
 )
 
 var Verbose bool
+var weathers = []string{"sunny", "windy", "cloudy", "rainy"}
 
 func main() {
 	cliApp := cli.NewApp()
@@ -15,7 +17,8 @@ func main() {
 	cliApp.Usage = "cli usage demo"
 	cliApp.Version = "0.0.1"
 	// 系统命令
-	cliApp.Commands = command.Commands()
+	cliApp.Commands = []*cli.Command{sayHelloCmd()}
+	//cliApp.Commands = command.Commands()
 	// 初始化之前调用
 	cliApp.Before = func(ctx *cli.Context) error {
 		fmt.Println("Before app run ...")
@@ -30,5 +33,60 @@ func main() {
 	if err != nil {
 		fmt.Printf("demo-cli execute error: %v\n", err)
 		os.Exit(-1)
+	}
+}
+
+func sayHelloCmd() *cli.Command {
+	return &cli.Command{
+		Name:    "hello",        // 命令名称，执行时需要指定
+		Aliases: []string{"ho"}, // 命令别名，简化名称
+		Usage:   "向您问好，-h 查看更多帮助信息",
+		Before: func(context *cli.Context) error {
+			fmt.Println("sayHello 命令 Before...")
+			return nil
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "n", Aliases: []string{"name"}, Usage: "您的姓名 `NAME`", Required: true},
+		},
+		Subcommands: cli.Commands{
+			&cli.Command{
+				Name:    "weather",     // 命令名称，执行时需要指定
+				Aliases: []string{"w"}, // 命令别名，简化名称
+				Usage:   "报告天气情况，-h 查看更多帮助信息",
+				Before: func(context *cli.Context) error {
+					fmt.Println("sayHello weatherCmd 子命令 Before...")
+					return nil
+				},
+				Flags: []cli.Flag{
+					//&cli.StringFlag{Name: "n", Aliases: []string{"name"}, Usage: "您的姓名 `NAME`", Required: true},
+				},
+				Action: func(ctx *cli.Context) error {
+					name := ctx.String("n")
+					rd := rand.New(rand.NewSource(time.Now().UnixNano()))
+					weatherCmd := weathers[rd.Intn(len(weathers))]
+					fmt.Printf("hello %s, today is a %s day!\n", name, weatherCmd)
+					return nil
+				},
+				Category: "weather",
+			},
+			&cli.Command{
+				Name:    "complain-weather", // 命令名称，执行时需要指定
+				Aliases: []string{"cw"},     // 命令别名，简化名称
+				Usage:   "Complains the weather today",
+				Before: func(ctx *cli.Context) error {
+					return nil
+				},
+				Flags: []cli.Flag{},
+				Action: func(ctx *cli.Context) error {
+					return nil
+				},
+				Category: "weather",
+			},
+		}, // 子命令
+		Action: func(ctx *cli.Context) error { // 具体命令的执行逻辑
+			name := ctx.String("n")
+			fmt.Println("hello,", name, "!")
+			return nil
+		},
 	}
 }
