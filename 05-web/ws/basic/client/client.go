1a75e36a7cde72114bd824a5ea73df8a06298009
hank <hankmo.x@gmail.com>
2025-03-21 03:44:49 +0000

    feat: add websocket demo to test weather data will be consumed even if server/client is closed

--- 05-web/ws/basic/client/client.go
+++ 05-web/ws/basic/client/client.go
@@ -26,48 +26,56 @@ func main() {
 
 	fmt.Println("conn response:", r)
 	// read asynchronously
-	go func() {
-		for {
-			mt, msg, err := conn.ReadMessage()
-			if err != nil {
-				log.Fatal("read message error:", err)
-			}
-			fmt.Printf("received message: %s, type: %d\n", msg, mt)
-			// fmt.Printf("received message: %s, type: %d", msg, websocket.FormatMessageType(mt))
-		}
-	}()
+	// go func() {
+	// 	for {
+	// 		mt, msg, err := conn.ReadMessage()
+	// 		if err != nil {
+	// 			log.Fatal("read message error:", err)
+	// 		}
+	// 		fmt.Printf("received message: %s, type: %d\n", msg, mt)
+	// 		// fmt.Printf("received message: %s, type: %d", msg, websocket.FormatMessageType(mt))
+	// 	}
+	// }()
 
 	// send message
-	ticker := time.NewTicker(time.Second * 2)
-	cnt := 0
-	defer ticker.Stop()
+	// ticker := time.NewTicker(time.Second * 2)
+	// cnt := 0
+	// defer ticker.Stop()
 	for {
-		select {
-		// 这里t为系统当前时间
-		case t := <-ticker.C:
-			err := conn.WriteMessage(websocket.TextMessage, []byte("当前时间: "+t.String()))
-			if err != nil {
-				log.Fatal("send message error:", err)
-				return
-			}
-			cnt++
-			// 主动关闭连接
-			if cnt > 5 {
-				return
-			}
-		case <-interrupt: // 接收到中断消息
-			fmt.Println("interrupt")
-			// 发送一个关闭消息然后等待服务端关闭连接， 服务端会收到 read: websocket: close 1000 (normal) 消息
-			if err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")); err != nil {
-				log.Println("write close:", err)
-			}
-			// 如果操作超时，退出客户端链接
-			// select {
-			// case <-time.After(time.Second):
-			// }
-			fmt.Println("连接中断")
-			time.Sleep(time.Second)
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Printf("received error: %v", err)
 			return
 		}
+		fmt.Println("Sleep 3s")
+		time.Sleep(time.Second * 3)
+		fmt.Printf("received message: %s\n", msg)
+		// select {
+		// // 这里t为系统当前时间
+		// case t := <-ticker.C:
+		// 	err := conn.WriteMessage(websocket.TextMessage, []byte("当前时间: "+t.String()))
+		// 	if err != nil {
+		// 		log.Fatal("send message error:", err)
+		// 		return
+		// 	}
+		// 	cnt++
+		// 	// 主动关闭连接
+		// 	if cnt > 5 {
+		// 		return
+		// 	}
+		// case <-interrupt: // 接收到中断消息
+		// 	fmt.Println("interrupt")
+		// 	// 发送一个关闭消息然后等待服务端关闭连接， 服务端会收到 read: websocket: close 1000 (normal) 消息
+		// 	if err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")); err != nil {
+		// 		log.Println("write close:", err)
+		// 	}
+		// 	// 如果操作超时，退出客户端链接
+		// 	// select {
+		// 	// case <-time.After(time.Second):
+		// 	// }
+		// 	fmt.Println("连接中断")
+		// 	time.Sleep(time.Second)
+		// 	return
+		// }
 	}
 }
--- 05-web/ws/basic/server.go
+++ 05-web/ws/basic/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -18,25 +20,41 @@ func main() {
 		}
 		defer conn.Close()
 		fmt.Println("客户端已经连接: ", conn.RemoteAddr().String())
+		cnt := 0
+		tk := time.NewTicker(time.Second)
+		defer tk.Stop()
 		for {
-			mt, msg, err := conn.ReadMessage()
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-				fmt.Println("客户端连接异常断开")
-				break
-			} else if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				fmt.Println("客户端主动断开连接")
-				break
-
-			} else {
-				if err != nil {
-					fmt.Printf("read message error: %v\n", err)
+			// _, msg, err := conn.ReadMessage()
+			// if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+			// 	fmt.Println("客户端连接异常断开")
+			// 	break
+			// } else if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+			// 	fmt.Println("客户端主动断开连接")
+			// 	break
+			//
+			// } else {
+			// 	if err != nil {
+			// 		fmt.Printf("read message error: %v\n", err)
+			// 		break
+			// 	}
+			// }
+			// 等待4秒测试当客户端推送结束并断开后，服务端是否还能继续接收数据，模拟处理数据延迟的情景
+			// websocket是可靠协议，可以接收客户端发过来的所有数据，即使客户端已经关闭
+			//          fmt.Println("wait 4s")
+			// time.Sleep(time.Second * 4)
+			// fmt.Printf("received message: %v\n", string(msg))
+			select {
+			case c := <-tk.C:
+				if err := conn.WriteMessage(websocket.TextMessage, []byte(c.String())); err != nil {
+					fmt.Printf("write message error: %v", err)
 					break
 				}
-			}
-			fmt.Printf("received message: %v\n", string(msg))
-			if err := conn.WriteMessage(mt, msg); err != nil {
-				fmt.Printf("write message error: %v", err)
-				break
+				cnt++
+				if cnt > 5 {
+                    // 关闭服务端，测试客户端是否可以继续收到数据
+                    //  可以, 推送的数据会消费完
+					os.Exit(-1)
+				}
 			}
 		}
 	})
